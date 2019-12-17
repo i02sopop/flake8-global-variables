7d10fa3f35d20f561b1c1442ea47ad4f6f58c6cd
Ritho <p.alvarez@zaleos.net>
2019-12-17 18:08:55 +0100

    Use the variables at __init__.py in setup.py.

--- setup.py
+++ setup.py
@@ -1,26 +1,36 @@
+import os
 import setuptools
 
-requires = [
-    "flake8 > 3.0.0",
-]
+base_dir = os.path.dirname(__file__)
+about = {}
+with open(os.path.join(base_dir, "flake8_global_variables", "__init__.py")) as f:
+    exec(f.read(), about)
 
 setuptools.setup(
-    name="flake8_global_variables",
+    name=about["__title__"],
     license="MIT",
-    version="0.1.0",
-    description="It shows an error when it finds a global variable defined",
-    author="Ritho",
-    author_email="p.alvarez@zaleos.net",
+    version=about["__version__"],
+    description=about["__summary__"],
+    author=about["__author__"],
+    author_email=about["__email__"],
+    maintainer=about['__maintainer__'],
+    maintainer_email=about['__maintainer_email__'],
     url="https://github.com/i02sopop/flake8-global-variables",
+
     packages=[
         "flake8_global_variables",
     ],
-    install_requires=requires,
+
+    install_requires=[
+        "flake8 > 3.0.0",
+    ],
+
     entry_points={
         'flake8.extension': [
-            'X = flake8_global_variables.globalvariables:GlobalVariables',
+            'W = flake8_global_variables.globalvariables:GlobalVariables',
         ],
     },
+
     classifiers=[
         "Framework :: Flake8",
         "Environment :: Console",
