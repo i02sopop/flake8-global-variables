1d4024ac0e3147994e1e95905494e1df08cf6d5c
Ritho <p.alvarez@zaleos.net>
2019-12-17 18:20:53 +0100

    Add a README and the plugin license.

--- flake8_global_variables/visitor.py
+++ flake8_global_variables/visitor.py
@@ -1,3 +1,20 @@
+# Copyright (C) 2019 Pablo Alvarez de Sotomayor Posadillo
+
+# This file is part of flake8_global_variables.
+
+# flake8_global_variables is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+
+# flake8_global_variables is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+#Public License for more details.
+
+# You should have received a copy of the GNU General Public License along with
+# flake8_global_variables. If not, see <http://www.gnu.org/licenses/>.
+
 import ast
 
 from collections import namedtuple
--- setup.py
+++ setup.py
@@ -1,3 +1,20 @@
+# Copyright (C) 2019 Pablo Alvarez de Sotomayor Posadillo
+
+# This file is part of flake8_global_variables.
+
+# flake8_global_variables is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+
+# flake8_global_variables is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+#Public License for more details.
+
+# You should have received a copy of the GNU General Public License along with
+# flake8_global_variables. If not, see <http://www.gnu.org/licenses/>.
+
 import os
 import setuptools
 
@@ -8,7 +25,7 @@ with open(os.path.join(base_dir, "flake8_global_variables", "__init__.py")) as f
 
 setuptools.setup(
     name=about["__title__"],
-    license="MIT",
+    license="GPLV3",
     version=about["__version__"],
     description=about["__summary__"],
     author=about["__author__"],
@@ -35,7 +52,7 @@ setuptools.setup(
         "Framework :: Flake8",
         "Environment :: Console",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Topic :: Software Development :: Libraries :: Python Modules",
