dda6979b6033e381fd76e120d5c66391dc50182f
Ritho <p.alvarez@zaleos.net>
2019-12-17 15:54:33 +0100

    Add an __init__.py definition with the package information.

---
+++ flake8_global_variables/__init__.py
@@ -0,0 +1,16 @@
+__title__ = "flake8-global-variables"
+__summary__ = (
+    "Flake8 plugin that checks if they are global variables defined."
+)
+__uri__ = "https://github.com/i02sopop/flake8-global-variables"
+
+__version__ = "0.1.0"
+
+__author__ = "Ritho"
+__email__ = "p.alvarez@zaleos.net"
+
+__maintainer__ = "Ritho"
+__maintainer_email__ = "p.alvarez@zaleos.net"
+
+__license__ = ""
+__copyright__ = "Copyright 2019 %s" % __author__
